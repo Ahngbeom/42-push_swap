7038ad714c5f364d781984825245e77a519b0b55
AhngBeom <bbu0704@gmail.com>
2021-08-22 03:38:49 +0900

    exeption processing

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,12 +6,43 @@
 /*   By: bahn <bbu0704@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 18:08:05 by bahn              #+#    #+#             */
-/*   Updated: 2021/08/21 15:36:13 by bahn             ###   ########.fr       */
+/*   Updated: 2021/08/22 03:01:06 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "get_next_line.h"
+
+static	void	select_operation(t_frame *frame, char *cmd)
+{
+	if (ft_strncmp(SWAP_A, cmd, ft_strlen(cmd)) == 0)
+		swap_a(frame);
+	else if (ft_strncmp(SWAP_B, cmd, ft_strlen(cmd)) == 0)
+		swap_b(frame);
+	else if (ft_strncmp(SWAP_S, cmd, ft_strlen(cmd)) == 0)
+		swap_s(frame);
+	else if (ft_strncmp(PUSH_A, cmd, ft_strlen(cmd)) == 0)
+		push_a(frame);
+	else if (ft_strncmp(PUSH_B, cmd, ft_strlen(cmd)) == 0)
+		push_b(frame);
+	else if (ft_strncmp(ROTATE_A, cmd, ft_strlen(cmd)) == 0)
+		rotate_a(frame);
+	else if (ft_strncmp(ROTATE_B, cmd, ft_strlen(cmd)) == 0)
+		rotate_b(frame);
+	else if (ft_strncmp(ROTATE_R, cmd, ft_strlen(cmd)) == 0)
+		rotate_r(frame);
+	else if (ft_strncmp(REVERSE_ROTATE_A, cmd, ft_strlen(cmd)) == 0)
+		reverse_rotate_a(frame);
+	else if (ft_strncmp(REVERSE_ROTATE_B, cmd, ft_strlen(cmd)) == 0)
+		reverse_rotate_b(frame);
+	else if (ft_strncmp(REVERSE_ROTATE_R, cmd, ft_strlen(cmd)) == 0)
+		reverse_rotate_r(frame);
+	else
+	{
+		ft_putendl_fd("Error", 1);
+		exit(1);
+	}
+}
 
 int	main(int argc, char *argv[])
 {
@@ -21,45 +52,19 @@ int	main(int argc, char *argv[])
 	invalid_arg(argc, argv);
 	frame = frame_init();
 	stack_init(frame, argv);
-
-	if (check_asc(frame->a) == TRUE)
-		ft_putendl_fd("OK", 1);
-	else
+	while (get_next_line(0, &cmd) > 0)
 	{
-		while (get_next_line(0, &cmd) > 0)
-		{
-			if (ft_strncmp(SWAP_A, cmd, ft_strlen(cmd)) == 0)
-				swap_a(frame);
-			if (ft_strncmp(SWAP_B, cmd, ft_strlen(cmd)) == 0)
-				swap_b(frame);
-			if (ft_strncmp(SWAP_S, cmd, ft_strlen(cmd)) == 0)
-				swap_s(frame);
-
-			if (ft_strncmp(PUSH_A, cmd, ft_strlen(cmd)) == 0)
-				push_a(frame);
-			if (ft_strncmp(PUSH_B, cmd, ft_strlen(cmd)) == 0)
-				push_b(frame);
-
-			if (ft_strncmp(ROTATE_A, cmd, ft_strlen(cmd)) == 0)
-				rotate_a(frame);
-			if (ft_strncmp(ROTATE_B, cmd, ft_strlen(cmd)) == 0)
-				rotate_b(frame);
-			if (ft_strncmp(ROTATE_R, cmd, ft_strlen(cmd)) == 0)
-				rotate_r(frame);
-
-			if (ft_strncmp(REVERSE_ROTATE_A, cmd, ft_strlen(cmd)) == 0)
-				reverse_rotate_a(frame);
-			if (ft_strncmp(REVERSE_ROTATE_B, cmd, ft_strlen(cmd)) == 0)
-				reverse_rotate_b(frame);
-			if (ft_strncmp(REVERSE_ROTATE_R, cmd, ft_strlen(cmd)) == 0)
-				reverse_rotate_r(frame);
-		}
+		select_operation(frame, cmd);
+		if (cmd != NULL)
+			free(cmd);
+		cmd = NULL;
 	}
 	if (check_asc(frame->a) == TRUE && frame->b == NULL)
 		ft_putendl_fd("OK", 1);
 	else
 		ft_putendl_fd("KO", 1);
-	free(cmd);
+	if (cmd != NULL)
+		free(cmd);
 	frame_free(frame);
 	return (0);
 }
--- srcs/check.c
+++ srcs/check.c
@@ -6,59 +6,87 @@
 /*   By: bahn <bbu0704@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 15:55:42 by bahn              #+#    #+#             */
-/*   Updated: 2021/07/22 20:28:15 by bahn             ###   ########.fr       */
+/*   Updated: 2021/08/22 03:35:47 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static  int ft_nbrlen(int num)
+{
+    int     i;
+    int     n;
+    
+    i = 1;
+    n = num;
+    if (num >= 0)
+    {
+        while ((n / 10)> 0)
+        {
+            i++;
+            n = n / 10;
+        }
+    }
+    else
+    {
+        ++i;
+        while ((n / 10) * -1 > 0)
+        {
+            i++;
+            n = n / 10;
+        }
+    }
+    return (i);
+}
 int     isInteger(char *str)
 {
     int     i;
 
     i = 0;
     if (str[i] == '-' || str[i] == '+')
-    ++i;
+        ++i;
     while (str[i] != '\0')
-    {
-            if (!ft_isdigit(str[i]))
-                    return (1);
-            i++;
+    {   
+        if (!ft_isdigit(str[i]))
+            return (1);
+        if (ft_strlen(str) != ft_nbrlen(ft_atoi(str)))
+            return (1);
+        i++;
     }
     return (0);
 }
 
 int     invalid_arg(int argc, char *argv[])
 {
-        int     i;
+    int     i;
 	int	j;
 
-        if (argc < 2)
+    if (argc < 2)
+    {
+        ft_putendl_fd("Error", 1);
+        exit(1);
+    }
+    i = 1;
+    while (argv[i] != NULL)
+    {
+        if (isInteger(argv[i]))
         {
-                printf("ERROR:::인자가 존재하지 않습니다.\n");
+            ft_putendl_fd("Error", 1);
+            exit(1);
+        }
+        j = i + 1;
+        while (argv[j] != NULL)
+        {
+            if (ft_strcmp(argv[i], argv[j]))
+            {
+                ft_putendl_fd("Error", 1);
                 exit(1);
+            }
+            j++;
         }
-        i = 1;
-        while (argv[i] != NULL)
-        {
-                if (isInteger(argv[i]))
-                {
-                        printf("ERROR:::인자가 올바르지 않습니다.\n");
-                        exit(1);
-                }
-		j = i + 1;
-		while (argv[j] != NULL)
-		{
-			if (ft_strcmp(argv[i], argv[j]))
-			{
-                printf("ERROR:::중복되는 인자가 존재합니다.\n");
-				exit(1);
-			}
-			j++;
-		}
-		i++;
-        }
-        return (i - 1);
+        i++;
+    }
+    return (i - 1);
 }
 
 int     check_asc(t_stack *stack)
